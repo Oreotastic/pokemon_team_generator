39b36076976a28e9e0f353a9c16a5cc394cdb97a
Oretastic <hloreair@outlook.com>
2020-03-04 20:50:21 -0500

    init

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const db = require('./db')
+const path = require('path')
+
+app.use(express.json())
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.get('/api/pokemon', (req, res, next) => {
+  db.getPokemon()
+  .then(response => res.send(response))
+  .catch(next)
+})
+
+app.post('/api/pokemon', (req, res, next) => {
+  db.createRow(req.body.name)
+  .then(response => res.send(response))
+  .catch(next)
+})
+
+const port = 3000
+
+db.sync().then(() => {
+  app.listen(port, ()=>{
+    console.log(`listening on port ${port}`)
+  })
+})
